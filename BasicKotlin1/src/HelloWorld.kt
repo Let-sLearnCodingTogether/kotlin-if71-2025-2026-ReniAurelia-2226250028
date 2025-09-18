5a6b48a023513425551398bbd6be67862d39ea69
ReniAurelia-2226250028 <reniaurelia_2226250028@mhs.mdp.ac.id>
2025-09-18 19:43:26 +0700

    Pertemuan 1-3

---
+++ BasicKotlin1/src/HelloWorld.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //menampilkan text ke layar
+    println("Hello World.")
+
+    print("Selamat malam, ")
+    print("Reni Aurelia")
+
+    println("2226250028")
+    print("Hello, ")
+    print("Reni Aurelia")
+}
---
+++ BasicKotlin1/src/ltihan.kt
@@ -0,0 +1,5 @@
+fun main() {
+    println("2226250028")
+    print("Hello, ")
+    print("Reni Aurelia")
+}
