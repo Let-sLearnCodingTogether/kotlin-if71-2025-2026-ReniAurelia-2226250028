70a588984954cb4115fbdf04f08a4272b628ce4a
ReniAurelia-2226250028 <reniaurelia_2226250028@mhs.mdp.ac.id>
2025-09-18 19:49:29 +0700

    Pertemuan 2

---
+++ Pertemuan 2/Latihan1.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val name : String = "Reni Aurelia"
+    val npm : Long = 2226250028
+    var age : Int = 0
+    age = 20
+
+    println("Mahasiswa dengan : $npm, memiliki nama : $name, memiliki usia : $age")
+}
---
+++ Pertemuan 2/Latihan2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val firstname : String = "Reni"
+    val lastname : String = "Aurelia"
+    var score : Int = 0
+    score = 100
+
+    println("Nama Lengkap : $firstname $lastname , Score: $score")
+}
